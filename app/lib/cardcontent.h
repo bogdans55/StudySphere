27ea18fa6a5ebbf78759c321b7b72c06b7871039
Bogdan Stojadinovic <bogdanstojadinovic5@gmail.com>
2023-11-21 19:23:48 +0100

    Changed std::string to QString in CardContent

--- app/lib/cardcontent.h
+++ app/lib/cardcontent.h
@@ -6,20 +6,20 @@
 class CardContent
 {
 public:
-    CardContent(std::string &text);
+    CardContent(QString &text);
     ~CardContent();
 
     void show();
 
     // Getters
-    inline std::string getText() const {return m_text;}
+    inline QString getText() const {return m_text;}
     inline QImage getImage() const {return m_image;}
 
     // Setter
     void setImage(const QImage &image);
 
 private:
-    std::string m_text;
+    QString m_text;
     QImage m_image;
 };
 
