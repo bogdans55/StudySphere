305fcc4c9cbbb5a2f542ace263f5667d68fcc800
marko1684 <mpetrovic.1684@gmail.com>
2023-12-02 02:26:38 +0100

    Added Server header
    Defined Server header with its attributes and methods...

--- app/StudySphereServer/Server.h
+++ app/StudySphereServer/Server.h
@@ -1,12 +1,4 @@
-#include <QTcpServer>
+#ifndef SERVER_H
+#define SERVER_H
 
-class Server : public QTcpServer {
-    Q_OBJECT
-
-public:
-    Server(QObject *parent = nullptr);
-
-protected:
-    void incomingConnection(qintptr socketDescriptor) override;
-
-};
+#endif // SERVER_H
