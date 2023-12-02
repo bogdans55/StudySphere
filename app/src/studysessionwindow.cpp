939213d6281faeab76478e324da189f324959398
kerkoc01 <mi20028@alas.matf.bg.ac.rs>
2023-12-02 04:41:23 +0100

    Implementing StudySessionWindow functionalities

--- app/src/studysessionwindow.cpp
+++ app/src/studysessionwindow.cpp
@@ -3,6 +3,15 @@
 
 StudySessionWindow::StudySessionWindow(QWidget *parent) :
     QWidget(parent),
+    m_session(new StudySession()),
+    ui(new Ui::StudySessionWindow)
+{
+    ui->setupUi(this);
+}
+
+StudySessionWindow::StudySessionWindow(QWidget *parent, const StudySession& session) :
+    QWidget(parent),
+    m_session(session),
     ui(new Ui::StudySessionWindow)
 {
     ui->setupUi(this);
@@ -15,30 +24,43 @@ StudySessionWindow::~StudySessionWindow()
 
 void StudySessionWindow::on_flipPushButton_clicked()
 {
-    // TODO flip card (show answer/question)
+    if(!session().getCurrentCard().answerShowed())
+        ui->cardTextEdit->setText(session().getCurrentCard().questionText());
+    else
+        ui->cardTextEdit->setText(session().getCurrentCard().questionAnswer());
+
+    session().getCurrentCard().flipCard();
 }
 
 
 void StudySessionWindow::on_skipPushButton_clicked()
 {
-    // TODO skip question and load next card
+    session().getCurrentCard().evaluateAnswer(0);
+    session().nextCard();
+    ui->cardTextEdit->setText(session().getCurrentCard().questionText());
 }
 
 
 void StudySessionWindow::on_badPushButton_clicked()
 {
-    // TODO save users grade and load next card
+    session().getCurrentCard().evaluateAnswer(1);
+    session().nextCard();
+    ui->cardTextEdit->setText(session().getCurrentCard().questionText());
 }
 
 
 void StudySessionWindow::on_midPushButton_clicked()
 {
-    // TODO save users grade and load next card
+    session().getCurrentCard().evaluateAnswer(2);
+    session().nextCard();
+    ui->cardTextEdit->setText(session().getCurrentCard().questionText());
 }
 
 
 void StudySessionWindow::on_goodPushButton_clicked()
 {
-    // TODO save users grade and load next card
+    session().getCurrentCard().evaluateAnswer(3);
+    session().nextCard();
+    ui->cardTextEdit->setText(session().getCurrentCard().questionText());
 }
 
