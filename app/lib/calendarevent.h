ca4caf4729b7e7b72d8dca5d52235d6f345e929d
Sofija001 <sofijavisnjic001@gmail.com>
2023-12-18 21:08:02 +0100

    Small fixes

--- app/lib/calendarevent.h
+++ app/lib/calendarevent.h
@@ -25,19 +25,5 @@ private:
     QDateTime m_dateTime;
 };
 
-QVariant CalendarEvent::toVariant() const
-{
-    QVariantMap map;
-    map.insert("event", m_eventText);
-    map.insert("dateTime", m_dateTime);
-    return map;
-}
-
-void CalendarEvent::fromVariant(const QVariant &variant)
-{
-    QVariantMap map = variant.toMap();
-    m_eventText = map.value("event").toString();
-    m_dateTime = map.value("dateTime").toDateTime();
-}
 
 #endif // CALENDAREVENT_H
--- app/src/calendarevent.cpp
+++ app/src/calendarevent.cpp
@@ -13,3 +13,17 @@ CalendarEvent::CalendarEvent(QString eventText, QDateTime dateTime)
 CalendarEvent::~CalendarEvent()
 {}
 
+QVariant CalendarEvent::toVariant() const
+{
+    QVariantMap map;
+    map.insert("event", m_eventText);
+    map.insert("dateTime", m_dateTime);
+    return map;
+}
+
+void CalendarEvent::fromVariant(const QVariant &variant)
+{
+    QVariantMap map = variant.toMap();
+    m_eventText = map.value("event").toString();
+    m_dateTime = map.value("dateTime").toDateTime();
+}
